9af8d33ac15a511ce0a213fd1eb14513e5fc357d
IdaresitEmmanuel <idaresitimmanuel@gmail.com>
2024-08-11 18:43:39 +0100

    Initial Commit

---
+++ naijakeyboard/naijakeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  naijakeyboardApp.swift
+//  naijakeyboard
+//
+//  Created by Hyebreed on 11/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct naijakeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
