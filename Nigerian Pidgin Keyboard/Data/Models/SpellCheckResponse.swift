3f80aa59c3418d417974cc059d2ae88281fd133f
IdaresitEmmanuel <idaresitimmanuel@gmail.com>
2024-08-26 01:19:42 +0100

    quick fix

--- Nigerian Pidgin Keyboard/Data/Models/SpellCheckResponse.swift
+++ Nigerian Pidgin Keyboard/Data/Models/SpellCheckResponse.swift
@@ -7,14 +7,9 @@
 
 import Foundation
 
-// This file was generated from JSON Schema using quicktype, do not modify it directly.
-// To parse the JSON, add this file to your project and do:
-//
-//   let welcome = try? JSONDecoder().decode(Welcome.self, from: jsonData)
-
 import Foundation
 
-// MARK: - Welcome
+// MARK: - SpellCheckResponse
 struct SpellCheckResponse: Codable {
     let software: Software
     let warnings: Warnings
